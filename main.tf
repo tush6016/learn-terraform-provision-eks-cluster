be80994bb0c3c5c89a6b776779456c2494662d5c
Tushar K <tushars6016@gmail.com>
2022-10-13 18:16:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,5 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "eks-cluster"
 }
